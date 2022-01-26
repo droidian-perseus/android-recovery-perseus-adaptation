8e07143e03368ab13774465a064db1e6b4154447
Thomas Hastings <thomas.hastings@tutanota.com>
2022-01-26 16:55:45 +0100

    Created user accessible scripts for fixing the wifi and adding extras

---
+++ data/usr/local/bin/fix-wifi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo ""
+echo "Welcome to Droidian on the Poco F1 (beryllium)."
+echo "Enabling WiFi..."
+sudo systemctl enable enable-ipa
+echo ""
+echo "WiFi is enabled, reboot is needed for it to take effect."
