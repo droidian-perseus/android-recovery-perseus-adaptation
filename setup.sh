131a4c6ebf6cec8ed38c72389962f8a0d094e0ef
Perseus X <xperseus2021@gmail.com>
2022-06-13 10:29:23 +0000

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -6,23 +6,26 @@ VENDOR_DEVICE_PROP=`grep ro.product.vendor.device /vendor/build.prop | cut -d "=
 
 # ui_print <text>
 ui_print() { echo -e "ui_print $1\nui_print" > $OUTFD; }
-ui_print "Authors: Joel Selvaraj, 1petro, Perseus X"
+ui_print "Authors: Joel Selvaraj, 1petro, Perseus X";
 
+## MIX 3 Droidian Adaptation Installer
 mkdir /r;
 
 # mount droidian rootfs
 mount /data/rootfs.img /r;
 
 # Apply bluetooth fix
-ui_print "Applying device adaptations..."
-cp -r data/* /r/
+ui_print "Applying device adaptations...";
+cp -r data/* /r/;
 
 # Changing permissions for extras script
-chmod +x /r/usr/local/bin/perseus-extras.sh
+chmod +x /r/usr/local/bin/perseus-extras.sh;
 
-# umount rootfs
+# Unmount rootfs
 umount /r;
 
-# flash boot.img
-flash_image /dev/block/bootdevice/by-name/boot boot.img
-flash_image /dev/block/bootdevice/by-name/dtbo dtbo.img
+# flash boot and dtbo
+flash_image /dev/block/bootdevice/by-name/boot boot.img;
+flash_image /dev/block/bootdevice/by-name/dtbo dtbo.img;
+
+## end install
