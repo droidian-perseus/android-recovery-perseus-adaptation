ed8b7ebe2f8accb917993e2f6322540759779b12
Perseus X <xperseus2021@gmail.com>
2022-06-13 06:29:58 +0000

    Adapted to perseus - Use default ofono - Removed enable-ipa.service - Removed rc.local - Removed first-boot.sh - Modified beryllium-extras.sh: rename to perseus and remove notch fix, add GAPPS to waydroid installer and use bardia mirror for faster download

--- data/usr/local/bin/first-boot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export PATH=/usr/bin:/usr/sbin
-chmod a+x /etc/rc.local
--- data/usr/local/bin/perseus-extras.sh
+++ data/usr/local/bin/perseus-extras.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-### Notch fix ###
-read -p "Apply notch-fix? Clock will be visible, other icon positions might break. [Y/n] " NOTCH
-if [ "$NOTCH" == "y" ] || [ "$NOTCH" == "Y" ]; then
-echo "Applying notch-fix..."
-cat >>  ~/.config/gtk-3.0/gtk.css<< EOF
-.phosh-topbar-clock {
-   margin-left: 190px;
-}
-
-.phosh-panel-btn > box {
-   margin-left: 0px;
-   margin-right: 0px;
-}
-
-.phosh-power-button {
-   margin-right: 50px;
-}
-EOF
-echo "Notch fix applied, reboot is needed tor it to take effect."
-else
-    echo "Skipping notch fix."
-fi
-
 ### Dark mode ###
 echo ""
 echo "Enable dark mode?"
@@ -49,15 +26,14 @@ if [ "$WAYDROID" == "y" ] || [ "$WAYDROID" == "Y" ]; then
     fi
     sudo apt install curl wget lxc python3 -y
     sudo apt install ca-certificates -y
-    export DISTRO="bullseye" && \
+    export DISTRO="bookworm" && \
     sudo curl -# --proto '=https' --tlsv1.2 -Sf https://repo.waydro.id/waydroid.gpg --output /usr/share/keyrings/waydroid.gpg && \
     echo "deb [signed-by=/usr/share/keyrings/waydroid.gpg] https://repo.waydro.id/ $DISTRO main" > ~/waydroid.list && \
     sudo mv ~/waydroid.list /etc/apt/sources.list.d/waydroid.list && \
     sudo apt update
     sudo apt -y install waydroid
-    sudo waydroid init
+    sudo waydroid init -s GAPPS -c https://waydroid.bardia.tech/OTA/system -v https://waydroid.bardia.tech/OTA/vendor
     sudo systemctl start waydroid-container
-#   sudo systemctl disable android_boot_completed.service
     echo ""
     echo "Installed Waydroid."
 else
